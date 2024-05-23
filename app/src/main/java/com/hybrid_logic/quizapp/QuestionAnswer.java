3ae06526f34a465db1fe69e56c749ffa6e746fe0
Jorge123Garcia <jorgeagcl217@gmail.com>
2024-05-22 19:35:00 -0600

    Initial commit

---
+++ app/src/main/java/com/hybrid_logic/quizapp/QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.hybrid_logic.quizapp;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "Which company owns Android?",
+            "Which one is not a programming language?",
+            "Where are you watching this?"
+
+
+
+    };
+
+    public static String choices[][]={
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Phyton"},
+            {"Facebook", "Whatsapp","Instagram","Youtube"}
+
+
+    };
+
+    public static String correctAnswers[]={
+            "Google",
+            "Notepad",
+            "Youtube"
+
+
+    };
+}
